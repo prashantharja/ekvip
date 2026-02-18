3c931196e8a210683a8bc33dee1a642c508f1c54
Prashanth Kumar Arja <parja@tractorsupply.com>
2026-02-18 22:06:33 +0530

    adding ekvip task

---
+++ src/com/ekvip/oop/UndoOperation.java
@@ -0,0 +1,19 @@
+package com.ekvip.oop;
+
+import java.util.Deque;
+
+public class UndoOperation {
+
+	int undoOperation(int value, Deque<Integer> history) {
+		
+		if (!history.isEmpty()) {
+			int previousValue = history.pop();
+			System.out.println("Undoing last operation. Restored value: " + previousValue);
+			return previousValue;
+		} else {
+			System.out.println("No operations to undo. Value remains: " + value);
+			return value;
+		}
+		
+	}
+}
